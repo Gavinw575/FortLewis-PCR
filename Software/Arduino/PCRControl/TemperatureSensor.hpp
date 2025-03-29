2b141d93493699100bdeccafccfba03c10f806ab
farjanashowme <farjana.showme.3@gmail.com>
2025-03-29 16:09:31 -0600

    Update TemperatureSensor.hpp

--- Software/Arduino/PCRControl/TemperatureSensor.hpp
+++ Software/Arduino/PCRControl/TemperatureSensor.hpp
@@ -4,28 +4,17 @@
 // TemperatureSensor class for reading temperature from a thermistor
 class TemperatureSensor {
   private:
-    int pin;
-
+  int pin;
+  
   public:
-    // Constructor: initializes the sensor pin
-    TemperatureSensor(int iPin) { 
-      pin = iPin; // The pin connected to the temperature sensor network
-    }
+  TemperatureSensor(int iPin) { 
+    pin = iPin; // the pin that conected to the tempature network
+  }
 
-    // Reads the temperature from the thermistor and applies corrections
-    double getTemp() { 
-      int tempReading = analogRead(pin);
-      double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
-      tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK ); // Convert to Kelvin
-      double tempC = tempK - 273.15; // Convert Kelvin to Celsius
-      return applyCorrection(tempC);
-    }
-    //Corrects the temperature based on the offset, updated on 03/28/25
-  private:
-    double applyCorrection(double temp) {
-      if (temp >= 94) return temp + 5 ;
-      else if (temp >= 80) return temp + 4;
-      else if (temp >= 70) return temp + 2;
-      return temp; // No correction below 70°C
-    }
+  double getTemp() { // returns the tempature from thermoristor connected to thermP in degrees C, includes noise reduction
+    int tempReading = analogRead(pin);
+    double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
+    tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK ); // kelvin
+    return (tempK - 273.15); // convert kelvin to celcius
+  }
 };
