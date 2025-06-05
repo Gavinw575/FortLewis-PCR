55c1d2f4ec30e21348b62ca05b75d7807bb42d96
Gavin Wakefield <130862917+Gavinw575@users.noreply.github.com>
2025-06-05 09:16:30 -0600

    Update PCRControl.ino

--- Software/Arduino/PCRControl/PCRControl.ino
+++ Software/Arduino/PCRControl/PCRControl.ino
@@ -1,5 +1,8 @@
+
 #include "TemperatureSensor.hpp"
 #include "PID.hpp"
+#include "RampPID.hpp"
+#include "HardcodedThreeStepPID.hpp"
 
 #include <math.h>
 
@@ -15,11 +18,8 @@ const int LidP = A1; // pin for thermal resistor conneccted to lid
 const int cPin = A2; // for curent recording
 const int ssr = 9; // solid state relay signal
 
-
 bool pPower = false; // software pielter on/off
 bool lPower = false; // software lid on/off
-//bool fanState = true; // fan starts ON
-
 
 bool verboseState = false; // spam serial with state every loop?
 bool verbosePID = false;  // spam serial with target and curent temperature?
@@ -29,7 +29,7 @@ int limitPWMH = 255;
 int limitPWMC = 255;
 
 float avgPTemp = 0; // last average for peltier temperature
-int avgPTempSampleSize = 50; // sample size for peltier temperature moving average
+int avgPTempSampleSize = 100; // sample size for peltier temperature moving average
 
 float avgPPWM = 0; // last average for peltier temperature
 int avgPPWMSampleSize = 2; // sample size for peltier temperature moving average was 4
@@ -37,212 +37,193 @@ int avgPPWMSampleSize = 2; // sample size for peltier temperature moving average
 double targetPeltierTemp = 29; // the tempature the system will try to move to, in degrees C
 double currentPeltierTemp; // the tempature curently read from the thermoristor connected to thermP, in degrees C
 
-double currentLidTemp;
+double currentLidTemp; 
 double LastLidTemp;
 
 // setup pieltier tempature sensor
 TemperatureSensor peltierT(thermP);
-TemperatureSensor LidT(LidP); // JD setup for thermo resistor temp
+TemperatureSensor LidT(LidP); // JD setup for thermo resistor temp 
 
 // setup pieltier PID
-PID peltierPID(5, 2, .5);
+PID peltierPID(10, 0.01, 1000000);
 
 void setup() {
-  // setup serial
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+// setup serial
+Serial.begin(115200);
+while (!Serial) {
+; // wait for serial port to connect. Needed for native USB port only
+}
 
-  // setup pins
-  pinMode(inA, OUTPUT);
-  pinMode(inB, OUTPUT);
-  pinMode(ppwm, OUTPUT);
-  pinMode(fpwm, OUTPUT);
-  pinMode(thermP, INPUT);
-  pinMode(LidP, INPUT);
-  pinMode(ssr, OUTPUT);
-  // set initial pin state to off
-  digitalWrite(inA, LOW);
-  digitalWrite(inB, LOW);
-  analogWrite(ppwm, 0);
-  analogWrite(fpwm, 0);
-  digitalWrite(ssr,LOW);
+// setup pins
+pinMode(inA, OUTPUT);
+pinMode(inB, OUTPUT);
+pinMode(ppwm, OUTPUT);
+pinMode(fpwm, OUTPUT);
+pinMode(thermP, INPUT);
+pinMode(LidP, INPUT);
+pinMode(ssr, OUTPUT);
+// set initial pin state to off
+digitalWrite(inA, LOW);
+digitalWrite(inB, LOW);
+analogWrite(ppwm, 0);
+analogWrite(fpwm, 0);
+digitalWrite(ssr,LOW);
 }
 
 // checks UART serial for any commands and executes them
 void handleSerialInput() {
-  if (Serial.available() > 0) {
-    String incomingCommand = Serial.readString();
-    if (incomingCommand == "whoami\n") { // print out software ID
-      Serial.print("FLC-PCR software version: ");
-      Serial.print(SOFTWARE_VERSION);
-      Serial.print("\n");
-    }
-    if (incomingCommand == "verbose\n") { // toggle sending curent temp, pwm and current lid temp every loop
-      if (verboseState) {
-        verboseState = false;
-      } else {
-        verboseState = true;
-      }
-    }
-    if (incomingCommand == "pid\n") { // toggle sending target temp, curent temp and pwm every loop
-      if (verbosePID) {
-        verbosePID = false;
-      } else {
-        verbosePID = true;
-      }
-    }
-    if (incomingCommand == "d\n") { // request a single sample of the curent temp, pwm and lid temp
-      // request system data
-      Serial.print(avgPTemp);
-      Serial.print(" ");
-      Serial.print(avgPPWM);
-      Serial.print(" ");
-      Serial.print(currentLidTemp);
-      Serial.print("\n");
-    }
-    if (incomingCommand == "state\n") { // check weather or not the pieltiers are on or off
-      Serial.print(pPower);
-      Serial.print("\n");
-    }
-    if (incomingCommand == "offl\n") { // turn off lid
-      lPower = false;
-    } else if (incomingCommand == "offp\n") { // turn off pieltiers
-      pPower = false;
-    } else if (incomingCommand == "off\n") { // turn off both lid and pieltiers
-      pPower = false;
-      lPower = false;
-    }
-    if (incomingCommand == "onl\n") { // turn on the lid
-      lPower = true;
-    } else if (incomingCommand == "onp\n") { // turn on the pieltiers
-      pPower = true;
-      peltierPID.reset();
-    } else if (incomingCommand == "on\n") { // turn on both lid and pieltiers
-      peltierPID.reset();
-      pPower = true;
-      lPower = true;
-    }
-    if (incomingCommand.substring(0,2) == "kp") { // set proportional gain
-      peltierPID.setKp(incomingCommand.substring(2).toFloat());
-    }
-    if (incomingCommand.substring(0,2) == "ki") { // set integral gain
-      peltierPID.setKi(incomingCommand.substring(2).toFloat());
-    }
-    if (incomingCommand.substring(0,2) == "kd") { // set derivitive gain
-      peltierPID.setKd(incomingCommand.substring(2).toFloat());
-    }
-    if (incomingCommand.substring(0,2) == "pt") { // set pieltier temperature
-      peltierPID.reset();
-      targetPeltierTemp = incomingCommand.substring(2).toFloat();
-    }
-    if (incomingCommand.substring(0,3) == "pia") { // set size of pieltier temperature low pass filter
-      avgPTempSampleSize = incomingCommand.substring(3).toInt();
-    }
-    if (incomingCommand.substring(0,3) == "poa") { // set size of pwm low pass filter
-      avgPPWMSampleSize = incomingCommand.substring(3).toInt();
-    }
-    if (incomingCommand.substring(0,3) == "plc") { // set the max pwm for coling
-      limitPWMC = incomingCommand.substring(3).toInt();
-    }
-    if (incomingCommand.substring(0,3) == "plh") { // set the max pwm for heating
-      limitPWMH = incomingCommand.substring(3).toInt();
-    }
-  }
+if (Serial.available() > 0) {
+String incomingCommand = Serial.readString();
+if (incomingCommand == "whoami\n") { // print out software ID
+Serial.print("FLC-PCR software version: ");
+Serial.print(SOFTWARE_VERSION);
+Serial.print("\n");
+}
+if (incomingCommand == "verbose\n") { // toggle sending curent temp, pwm and current lid temp every loop
+if (verboseState) {
+verboseState = false;
+} else {
+verboseState = true;
+}
+}
+if (incomingCommand == "pid\n") { // toggle sending target temp, curent temp and pwm every loop
+if (verbosePID) {
+verbosePID = false;
+} else {
+verbosePID = true;
+}
+}
+if (incomingCommand == "d\n") { // request a single sample of the curent temp, pwm and lid temp
+// request system data
+Serial.print(avgPTemp);
+Serial.print(" ");
+Serial.print(avgPPWM);
+Serial.print(" ");
+Serial.print(currentLidTemp);
+Serial.print("\n");
+}
+if (incomingCommand == "state\n") { // check weather or not the pieltiers are on or off
+Serial.print(pPower);
+Serial.print("\n");
+}
+if (incomingCommand == "offl\n") { // turn off lid
+lPower = false;
+} else if (incomingCommand == "offp\n") { // turn off pieltiers
+pPower = false;
+} else if (incomingCommand == "off\n") { // turn off both lid and pieltiers
+pPower = false;
+lPower = false;
+}
+if (incomingCommand == "onl\n") { // turn on the lid
+lPower = true;
+} else if (incomingCommand == "onp\n") { // turn on the pieltiers
+pPower = true;
+peltierPID.reset();
+} else if (incomingCommand == "on\n") { // turn on both lid and pieltiers
+peltierPID.reset();
+pPower = true;
+lPower = true;
+}
+if (incomingCommand.substring(0,2) == "kp") { // set proportional gain
+peltierPID.setKp(incomingCommand.substring(2).toFloat());
+}
+if (incomingCommand.substring(0,2) == "ki") { // set integral gain
+peltierPID.setKi(incomingCommand.substring(2).toFloat());
+}
+if (incomingCommand.substring(0,2) == "kd") { // set derivitive gain
+peltierPID.setKd(incomingCommand.substring(2).toFloat());
+}
+if (incomingCommand.substring(0,2) == "pt") { // set pieltier temperature
+peltierPID.reset();
+targetPeltierTemp = incomingCommand.substring(2).toFloat();
+}
+if (incomingCommand.substring(0,3) == "pia") { // set size of pieltier temperature low pass filter
+avgPTempSampleSize = incomingCommand.substring(3).toInt();
+}
+if (incomingCommand.substring(0,3) == "poa") { // set size of pwm low pass filter
+avgPPWMSampleSize = incomingCommand.substring(3).toInt();
+}
+if (incomingCommand.substring(0,3) == "plc") { // set the max pwm for coling
+limitPWMC = incomingCommand.substring(3).toInt();
+}
+if (incomingCommand.substring(0,3) == "plh") { // set the max pwm for heating
+limitPWMH = incomingCommand.substring(3).toInt();
+}
+}
 }
 
 void loop() {
-  handleSerialInput();
+handleSerialInput();
 
-  currentLidTemp = LidT.getTemp(70); // read lid temp
-  currentPeltierTemp = peltierT.getTemp(targetPeltierTemp); // read pieltier temp
-  if (isnan(currentPeltierTemp) || isinf(currentPeltierTemp)) { // reset nan and inf values
-    currentPeltierTemp = avgPTemp;
-  }
-
-  // old temperature calibration
-  //currentPeltierTemp = 0.9090590064070043 * currentPeltierTemp + 3.725848396176527; // estimate vial temperature
-  //currentPeltierTemp = 0.6075525829531135 * currentPeltierTemp + 15.615801552818361; // seccond estimate
-
-  // 3/2/2021 temperature calabration
-  currentPeltierTemp = currentPeltierTemp + 2 ;
- 
-  avgPTemp = ((avgPTempSampleSize - 1) * avgPTemp + currentPeltierTemp) / avgPTempSampleSize; // average
-  double error = targetPeltierTemp - avgPTemp;
-  if (targetPeltierTemp < 70) {
-    peltierPID.setKp(5);
-    peltierPID.setKi(2);
-    peltierPID.setKd(.5);
-  } else {
-    peltierPID.setKp(5);
-    peltierPID.setKi(2);
-    peltierPID.setKd(.5);
-  }
-
-  peltierPWM = peltierPID.calculate(avgPTemp, targetPeltierTemp); // calculate pid and set to output
-
-  if (error > 0.5 && error < 8  && avgPTemp > 80) {
-    peltierPWM += 60;  // force a push to get over the hump
-  }
-
-  peltierPWM = min(limitPWMH, max(-limitPWMC, peltierPWM)); // clamp output between -255 and 255
-  if (isnan(peltierPWM ) || isinf(peltierPWM )) { // reset nan and inf values
-    peltierPWM = avgPPWM;
-  }
-  avgPPWM = ((avgPPWMSampleSize - 1) * avgPPWM + peltierPWM) / avgPPWMSampleSize; // average
-
-  // print out verbose data to serial if set
-  if (verboseState) {
-    Serial.print(avgPTemp);
-    Serial.print(" ");
-    Serial.print(avgPPWM);
-    Serial.print(" ");
-    Serial.print(currentLidTemp);
-    Serial.print("\n");
-  }
-  if (verbosePID) {
-    Serial.print(avgPTemp);
-    Serial.print(" ");
-    Serial.print(targetPeltierTemp);
-    Serial.print(" ");
-    Serial.print(avgPPWM);
-    Serial.print("\n");
-  }
-
-  // lid controll
-  if (lPower) {
-    if(currentLidTemp < 110){
-      digitalWrite(ssr, HIGH);
-    } else {
-      digitalWrite(ssr, LOW);
-    }
-  } else {
-    digitalWrite(ssr, LOW);
-  }
- 
-  
-  if (!pPower || currentPeltierTemp > 150) { // pieltiers on, shut down if over 150C
-    digitalWrite(inA, LOW);
-    digitalWrite(inB, LOW);
-    analogWrite(fpwm, 225);
-    analogWrite(ppwm, 0);
-
-    peltierPID.reset();
-    return;
-  } else { // pieltiers on
-    // convert pieltierDelta to pwm, inA, inB
-    analogWrite(ppwm, abs(peltierPWM));
-    if (peltierPWM > 0) {
-      digitalWrite(inA, HIGH);
-      digitalWrite(inB, LOW);
-      analogWrite(fpwm, 0); // Fan OFF
-    } else {
-      digitalWrite(inA, LOW);
-      digitalWrite(inB, HIGH);
-      analogWrite(fpwm, 255); // Full fan speed
-
-    }
-  }
+currentLidTemp = LidT.getTemp(); // read lid temp
+currentPeltierTemp = peltierT.getTemp(); // read pieltier temp
+if (isnan(currentPeltierTemp) || isinf(currentPeltierTemp)) { // reset nan and inf values
+currentPeltierTemp = avgPTemp;
 }
 
+// old temperature calibration
+//currentPeltierTemp = 0.9090590064070043 * currentPeltierTemp + 3.725848396176527; // estimate vial temperature
+//currentPeltierTemp = 0.6075525829531135 * currentPeltierTemp + 15.615801552818361; // seccond estimate
+
+// 3/2/2021 temperature calabration
+currentPeltierTemp = 1.1201 * currentPeltierTemp - 3.32051;
+
+avgPTemp = ((avgPTempSampleSize - 1) * avgPTemp + currentPeltierTemp) / avgPTempSampleSize; // average
+peltierPWM = peltierPID.calculate(avgPTemp, targetPeltierTemp); // calculate pid and set to output
+peltierPWM = min(limitPWMH, max(-limitPWMC, peltierPWM)); // clamp output between -255 and 255
+if (isnan(peltierPWM ) || isinf(peltierPWM )) { // reset nan and inf values
+peltierPWM = avgPPWM;
+}
+avgPPWM = ((avgPPWMSampleSize - 1) * avgPPWM + peltierPWM) / avgPPWMSampleSize; // average
+
+// print out verbose data to serial if set
+if (verboseState) {
+Serial.print(avgPTemp);
+Serial.print(" ");
+Serial.print(avgPPWM);
+Serial.print(" ");
+Serial.print(currentLidTemp);
+Serial.print("\n");
+}
+if (verbosePID) {
+Serial.print(avgPTemp);
+Serial.print(" ");
+Serial.print(targetPeltierTemp);
+Serial.print(" ");
+Serial.print(avgPPWM);
+Serial.print("\n");
+}
+
+// lid controll
+if (lPower) {
+if(currentLidTemp < 70){ 
+digitalWrite(ssr, HIGH);
+} else {
+digitalWrite(ssr, LOW);
+}
+} else {
+digitalWrite(ssr, LOW);
+}
+
+// pieltier control
+if (!pPower || currentPeltierTemp > 150) { // pieltiers on, shut down if over 150C
+digitalWrite(inA, LOW);
+digitalWrite(inB, LOW);
+analogWrite(fpwm, 225);
+analogWrite(ppwm, 0);
+
+peltierPID.reset();
+return;
+} else { // pieltiers on
+// convert pieltierDelta to pwm, inA, inB
+analogWrite(ppwm, abs(peltierPWM));
+analogWrite(fpwm, 255);
+if (peltierPWM > 0) {
+digitalWrite(inA, HIGH);
+digitalWrite(inB, LOW);
+} else {
+digitalWrite(inA, LOW);
+digitalWrite(inB, HIGH);
+}
+}
+}
